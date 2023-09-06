c06c6344d9c116ff010af01c56075d66f3f4d553
Steven Xie <xies1999@hotmail.com>
2023-09-06 14:01:46 -0400

    successfully added controlled form and used context

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import { useState, useEffect } from "react"
-import Gallery from "./components/Gallery"
-import SearchBar from "./components/SearchBar"
-import { DataContext } from "./context/DataContext"
+import { useEffect, useState, useRef } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+import { SearchContext } from './context/SearchContext'
 
-function App() {
-  let [message, setMessage] = useState("Seach for music")
-  let [search, setSearch] = useState("")
-  let [data, setData] = useState([])
+const App = () => {
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
+    let searchInput = useRef('')
 
-  const API_URL = "https://itunes.apple.com/search?term="
+    const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage("Not Found")
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        const fetchData = async () => {
+            document.title = `${term} music`
+            const response = await fetch(API_URL + term)
+            const resData = await response.json()
+            if (resData.results.length > 0) {
+                return setData(resData.results)
+            } else {
+                return setMessage('Not Found.')
+            }
         }
-      }
-      fetchData()
+        fetchData()
     }
-  }, [search])
 
-  const handleSearch = (e, term) => {
-    e.preventDefault()
-    setSearch(term)
-  }
-
-  return (
-    <div className="App">
-      {message}
-      <SearchBar handleSearch={handleSearch} />
-      <DataContext.Provider value={data}>
-      <Gallery/>
-      </DataContext.Provider>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar/>
+            </SearchContext.Provider>
+            {message}
+            <DataContext.Provider value={data}>
+                <Gallery/>
+            </DataContext.Provider>
+        </div>
+    )
 }
 
 export default App
+
+
+
+
+
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
