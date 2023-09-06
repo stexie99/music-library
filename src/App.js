f1d96899cce81c3e9a827d816126b4f957fae80f
Steven Xie <xies1999@hotmail.com>
2023-09-06 14:03:49 -0400

    updated default branch

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
-import { useEffect, useState, useRef } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
-import { SearchContext } from './context/SearchContext'
-
-const App = () => {
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-    let searchInput = useRef('')
-
-    const API_URL = 'https://itunes.apple.com/search?term='
-
-    const handleSearch = (e, term) => {
-        e.preventDefault()
-        const fetchData = async () => {
-            document.title = `${term} music`
-            const response = await fetch(API_URL + term)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-                return setData(resData.results)
-            } else {
-                return setMessage('Not Found.')
-            }
-        }
-        fetchData()
+import {useState, useEffect} from 'react'
+import Gallery from './components/Gallery';
+import SearchBar from './components/SearchBar';
+function App() {
+  let [message, setMessage] = useState('Seach for music')
+  let [search, setSearch] = useState('')
+  let [data, setData] = useState([])
+  const API_URL = 'https://itunes.apple.com/search?term='
+  useEffect(()=>{
+    if(search){
+    const fetchData = async () => {
+      document.title = `${search} music`
+      const response = await fetch(API_URL + search)
+      const resData = await response.json()
+      if(resData.results.length > 0){
+        setData(resData.results)
+      }else{
+        setMessage('Not Found')
+      }
     }
-
-    return (
-        <div className='App'>
-            <SearchContext.Provider value={{
-                term: searchInput,
-                handleSearch: handleSearch
-            }}>
-                <SearchBar/>
-            </SearchContext.Provider>
-            {message}
-            <DataContext.Provider value={data}>
-                <Gallery/>
-            </DataContext.Provider>
-        </div>
-    )
+    fetchData()
+    }
+  }, [search])
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term)
+  }
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch}/>
+      {message}
+      <Gallery data={data}/>
+    </div>
+  );
 }
-
-export default App
-
+export default App;
 
 
 
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,7 +1,26 @@
 import { useState } from 'react'
 
-function GalleryItem(props){
+function GalleryItem(props) {
     let [view, setView] = useState(false)
+
+    const simpleView = () => {
+        return (
+            <div style={simpleStyle}>
+                <h3>{props.item.trackName}</h3>
+                <h3>{props.item.collectionName}</h3>
+            </div>
+        )
+    }
+    const detailView = () => {
+        return (
+            <div style={detailStyle}>
+                <h3>{props.item.trackName}</h3>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
 
     const simpleStyle = {
         'width': '25vw',
@@ -21,32 +40,12 @@ function GalleryItem(props){
         'color': 'yellow'
     }
     
-    const simpleView=()=>{
-        return (
-            <div style={simpleStyle}>
-                <h3>{props.item.trackName}</h3>
-                <h4>{props.item.collectionName}</h4>
-            </div>
-        )
-    }
-
-    const detailView=()=>{
-        return(
-            <div style={detailStyle}>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
-            </div>
-        )
-
-    }
-
-    return(
-        <div onClick={()=>setView(!view)} style={{'display':'inline-block'}}>
-            {view ? detailView() : simpleView()}
-        </div>
-    )
+  return (
+    <div onClick={() => setView(!view)} 
+    style={{'display': 'inline-block'}}>
+    {view ? detailView() : simpleView()}
+    </div>
+  )
 }
 
 export default GalleryItem
