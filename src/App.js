aa3d40bfaba4c5ead3eb92cdd5c0925e05635d06
Steven Xie <xies1999@hotmail.com>
2023-09-07 00:10:50 -0400

    implemented suspense to music library interface

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, Suspense } from 'react'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import Spinner from './components/Spinner';
+import { createResource as fetchData } from './helper'
+
+
 function App() {
-  let [message, setMessage] = useState('Seach for music')
-  let [search, setSearch] = useState('')
-  let [data, setData] = useState([])
-  const API_URL = 'https://itunes.apple.com/search?term='
-  useEffect(()=>{
-    if(search){
-    const fetchData = async () => {
-      document.title = `${search} music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if(resData.results.length > 0){
-        setData(resData.results)
-      }else{
-        setMessage('Not Found')
-      }
+    let [message, setMessage] = useState('Seach for music')
+    let [search, setSearch] = useState('')
+    let [data, setData] = useState(null)
+    const API_URL = 'https://itunes.apple.com/search?term='
+    useEffect(() => {
+        if (search) {
+            setData(fetchData(search))
+        }
+    }, [search])
+
+    const handleSearch = (e, term) => {
+        e.preventDefault();
+        setSearch(term)
     }
-    fetchData()
+
+    const renderGallery = () => {
+        if(data){
+            return (
+                <Suspense fallback={<Spinner />}>
+                    <Gallery data={data} />
+                </Suspense>
+            )
+        }
     }
-  }, [search])
-  const handleSearch = (e, term) => {
-    e.preventDefault();
-    setSearch(term)
-  }
-  return (
-    <div className="App">
-      <SearchBar handleSearch={handleSearch}/>
-      {message}
-      <Gallery data={data}/>
-    </div>
-  );
+    
+    return (
+        <div className="App">
+            <SearchBar handleSearch={handleSearch} />
+            {message}
+            {renderGallery()}
+        </div>
+    )
+    
 }
 export default App;
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,19 @@
-import React from 'react'
 import GalleryItem from './GalleryItem'
 
-function Gallery(props) {
-    const display = props.data.map((item, i)=> {
+const Gallery = (props) => {
+    const data = props.data.result.read()
+
+    const display = data.map((item, index) => {
         return (
-            <GalleryItem item={item} key={i}/>
+            <GalleryItem item={item} key={index} />
         )
     })
 
     return (
-    <div>
-        {display}
-    </div>
-  )
+        <div>
+            {display}
+        </div>
+    )
 }
 
 export default Gallery
